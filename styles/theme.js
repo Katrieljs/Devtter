37dab172dd16fc96bc872e2f9af0a30ceba9c780
Katriel <100242891+Katrieljs@users.noreply.github.com>
2022-07-20 13:12:59 -0500

    Add files via upload

---
+++ styles/theme.js
@@ -0,0 +1,13 @@
+export const breakpoints = {
+  tablet: "800px",
+  mobile: "520px",
+}
+
+export const fonts = {
+  base: "system-ui",
+}
+
+export const colors = {
+  primary: "#9933ff",
+  secondary: "#999fff",
+}
